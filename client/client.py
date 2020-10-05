21426957ecd64a97e7279951d7083ad125dd940b
khaledismaeel <k.ismaeel@innopolis.university>
2020-10-05 12:31:15 +0300

    Started writing minimalist client code. Renamed server to name_server.

---
+++ client/client.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import json
+
+if __name__ == '__main__':
+    print(f'Running client.')
+
+    os.chdir(sys.path[0])
+    
+    with open('./config.json', 'r') as config_file:
+        config = json.load(config_file)
+    
+    try:
+        name_server = sys.argv[sys.argv.index('--name-server') + 1]
+    except ValueError:
+        name_server = config['default_name_server']
+    except IndexError:
+        print(f'No value for --name-server')
+        exit(1)
+    
+    
