86ee399cff3a18f49a5f9a75ac685ec81781db84
khaledismaeel <k.ismaeel@innopolis.university>
2020-10-05 23:29:42 +0300

    Changed file tree architecture. Added storage server.

--- name_server/name_server.py
+++ name_server/name_server.py
@@ -1,10 +1,25 @@
-import sys
-import random
-import string
-import utilities as utl
+import os
+import socket
+from threading import Thread
+import json
 
-storage_servers = []
+class ClientHandler(Thread):
+    def __init__(self, sock):
+        super().__init__(daemon = True)
     
+    def run(self):
+        pass
+
 if __name__ == '__main__':
-    tree = utl.filesystem()
-    tree.print_tree()   
+    with open('config.json', 'r') as config_file:
+        config = 
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 8800))
+    sock.listen()
+
+    while True:
+        connection, addr = sock.accept()
+        print(connection)
+    
+    sock.close()
