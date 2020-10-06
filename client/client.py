001e162383b43cc344da35b8da615db6bd50e800
Hussein <hussein_younes2015@hotmail.com>
2020-10-06 03:11:25 +0300

    implemented file creation

--- client/client.py
+++ client/client.py
@@ -2,54 +2,39 @@ import socket
 import tqdm
 import os
 import sys
+import json
+import argparse
 
 SEPARATOR = "<SEPARATOR>"
-BUFFER_SIZE = 4096 # send 4096 bytes each time step
+BUFFER_SIZE = 1024 # send 4096 bytes each time step
+
 
 command_type = sys.argv[1]
 command = sys.argv[2]
-params = map(float, sys.argv[3].strip('[]').split(','))
-    
-'''
-# the ip address or hostname of the receiver, AWS instance in this case
-host = sys.argv[2] # The IP of the instance
+params = sys.argv[3:] if len(sys.argv) > 3 else None
+print(type(params))
 
-# the port, specified as a paramter
-port = int(sys.argv[3])
 
-# the name of file we want to send
-filename = sys.argv[1]
+data = {}
+data["command_type"] = command_type
+data["command"] = command
+data["params"] = params
+json_data = json.dumps(data)
 
-# get the file size
-filesize = os.path.getsize(filename)
-'''
 
-# create the client socket, TCP socket
+host = "10.91.53.24" # The IP of the instance
+port = 8800
+
 s = socket.socket()
 
 print(f"[+] Connecting to {host}:{port}")
 s.connect((host, port))
 print("[+] Connected.")
 
-# send the filename and filesize
-s.send(f"{filename}{SEPARATOR}{filesize}".encode())
-filename = "hussein"
-filesize = "younes"
-print(f"{filename}{SEPARATOR}{filesize}")
+s.send(json_data.encode())
+print(json_data.encode())
 
-# start sending the file
-progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename, "rb") as f:
-    for _ in progress:
-        # read the bytes from the file
-        bytes_read = f.read(BUFFER_SIZE)
-        if not bytes_read:
-            # file transmitting is done
-            break
-        # we use sendall to assure transimission in
-        # busy networks
-        s.sendall(bytes_read)
-        # update the progress bar
-        progress.update(len(bytes_read))
-# close the sockets
+received = s.recv(BUFFER_SIZE).decode()
+print(received)
+
 s.close()
---
+++ storage_server/storage_server.py
@@ -0,0 +1,46 @@
+import socket
+import tqdm
+import os
+import json
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 8800
+
+BUFFER_SIZE = 1024
+SEPARATOR = "<SEPARATOR>"
+
+root_dir = "/home/hussein/dfs_dir"
+
+
+def create(path):
+    filepath = root_dir + path
+    if not os.path.exists(os.path.dirname(filepath)):
+        os.makedirs(os.path.dirname(filepath))
+    try:
+        with open(filepath, "w") as file:
+            pass
+        return {"message": "File Created Successfully"}
+    except:
+        return {"message": f"Could not create file: {filepath}"}
+
+
+if __name__ == '__main__':
+    s = socket.socket()
+    s.bind((SERVER_HOST, SERVER_PORT))
+
+    s.listen(5)
+    print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+    client_socket, address = s.accept()
+    print(f"[+] {address} is connected.")
+
+    data = client_socket.recv(BUFFER_SIZE).decode()
+    data = json.loads(data)
+
+    if data["command"] == "create":
+        print(data["params"][0])
+        print(json.dumps(create(data["params"][0])))
+        client_socket.send(json.dumps(create(data["params"][0])).encode())
+
+    client_socket.close()
+    s.close()
