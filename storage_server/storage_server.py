3c68388e5885f9ef3d7befb6cecb852b3e6e0490
Hussein <hussein_younes2015@hotmail.com>
2020-10-06 17:17:05 +0300

    Implemented download_file/register_server in storage_server

--- storage_server/storage_server.py
+++ storage_server/storage_server.py
@@ -1,6 +1,4 @@
-import socket
-import os
-import json
+import socket, os, json, sys, tqdm
 
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 8800
@@ -11,7 +9,22 @@ SEPARATOR = "<SEPARATOR>"
 root_dir = "/home/hussein/dfs_dir"
 
 
-def create(path):
+def connect_to_name_server(socket):
+    print(f"[+] Connecting to {SERVER_HOST}:{SERVER_HOST}")
+    socket.connect((SERVER_HOST, SERVER_PORT))
+    print("[+] Connected.")
+    command = sys.argv[1]
+    param = sys.argv[2]
+    data = {"command": command, "param": param}
+    json_data = json.dumps(data)
+    s.send(json_data.encode())
+
+    # receive response from the name server
+    received_msg = s.recv(BUFFER_SIZE).decode()
+    print(received_msg)
+
+
+def create_file(path):
     filepath = root_dir + path
     if not os.path.exists(os.path.dirname(filepath)):
         os.makedirs(os.path.dirname(filepath))
@@ -23,11 +36,31 @@ def create(path):
         return {"message": f"Could not create file: {filepath}"}
 
 
+def receive_file(socket, path, filesize):
+    filepath = root_dir + path
+    progress = tqdm.tqdm(range(filesize), f"Receiving {path}", unit="B", unit_scale=True, unit_divisor=1024)
+    try:
+        with open(filepath, "wb") as f:
+            for _ in progress:
+                bytes_read = socket.recv(BUFFER_SIZE)
+                if not bytes_read:
+                    break
+
+                f.write(bytes_read)
+                progress.update(len(bytes_read))
+        return {"message": "File Dowonloaded Successfully"}
+    except:
+        return {"message": f"Could not download file: {path}"}
+
+
 if __name__ == '__main__':
+    registration_sock = socket.socket()
+    connect_to_name_server(registration_sock)
+    registration_sock.close()
+
     s = socket.socket()
     s.bind((SERVER_HOST, SERVER_PORT))
     s.listen(5)
-
     while True:
         print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
         client_socket, address = s.accept()
@@ -38,8 +71,11 @@ if __name__ == '__main__':
 
         if data["command"] == "create":
             print(data["params"][0])
-            print(json.dumps(create(data["params"][0])))
-            client_socket.send(json.dumps(create(data["params"][0])).encode())
+            print(json.dumps(create_file(data["params"][0])))
+            client_socket.send(json.dumps(create_file(data["params"][0])).encode())
+
+        if data["command"] == "write":
+            file = data["params"][0]
 
         client_socket.close()
     s.close()
