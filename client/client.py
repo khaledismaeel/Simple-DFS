b720a82f2a946d88eaf8e3071dceb6d12aaff545
Hussein <hussein_younes2015@hotmail.com>
2020-10-07 04:35:38 +0300

    edited message logging

--- client/client.py
+++ client/client.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
         s.send(json.dumps(data).encode())
 
     received = s.recv(BUFFER_SIZE).decode()
-    print("==================================\nServer response...")
+    print("\n\n==================================================\nServer response:")
     print(received)
 
     s.close()
